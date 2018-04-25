b88cc0cdf6942b4d327995d324d2b5725532a342
Shehanka <hcsperera@outlook.com>
2018-04-25 23:55:25 +0530

    New files were added

--- RentLioClient/src/com/chamodshehanka/rentLioClient/view/controller/ManageVehicleUIController.java
+++ RentLioClient/src/com/chamodshehanka/rentLioClient/view/controller/ManageVehicleUIController.java
@@ -1,6 +1,9 @@
 package com.chamodshehanka.rentLioClient.view.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,10 +14,50 @@ import java.util.ResourceBundle;
  **/
 public class ManageVehicleUIController implements Initializable{
 
+    @FXML
+    private JFXComboBox<String> cmbVehicleType;
 
+    @FXML
+    private ImageView imgVehicle;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        setUpUIComponents();
+    }
+
+    @FXML
+    private void addAction() {
 
     }
+
+    @FXML
+    private void searchById() {
+
+    }
+
+    @FXML
+    private void updateAction() {
+
+    }
+
+    @FXML
+    private void deleteAction() {
+
+    }
+
+    private void setUpUIComponents(){
+        cmbVehicleType.getItems().addAll("Family","Other");
+    }
+
+    @FXML
+    private void browseImgURL(){}
+
+    @FXML
+    private void removeImgURL(){}
+
+    @FXML
+    private void getSelectedItem() {
+
+    }
+
 }
