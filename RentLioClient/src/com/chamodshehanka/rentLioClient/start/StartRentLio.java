27b24955aa334d851b8a0195cb47ce350e0385d2
Shehanka <hcsperera@outlook.com>
2018-04-26 17:37:03 +0530

    file chooser added

--- RentLioClient/src/com/chamodshehanka/rentLioClient/start/StartRentLio.java
+++ RentLioClient/src/com/chamodshehanka/rentLioClient/start/StartRentLio.java
@@ -21,7 +21,7 @@ public class StartRentLio extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("/com/chamodshehanka/rentLioClient/view/fxml/ManageBranchUI.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("/com/chamodshehanka/rentLioClient/view/fxml/ManageVehicleUI.fxml"));
         Scene scene = new Scene(parent);
 
         parent.setOnMousePressed(event -> {
--- RentLioClient/src/com/chamodshehanka/rentLioClient/view/controller/ManageVehicleUIController.java
+++ RentLioClient/src/com/chamodshehanka/rentLioClient/view/controller/ManageVehicleUIController.java
@@ -1,11 +1,19 @@
 package com.chamodshehanka.rentLioClient.view.controller;
 
+import com.chamodshehanka.rentLioClient.util.AlertBuilder;
+import com.chamodshehanka.rentLioCommon.dto.VehicleDTO;
 import com.jfoenix.controls.JFXComboBox;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.ResourceBundle;
 
 /**
@@ -20,6 +28,9 @@ public class ManageVehicleUIController implements Initializable{
     @FXML
     private ImageView imgVehicle;
 
+    private Stage fileChooseStage;
+    private String fileName;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setUpUIComponents();
@@ -27,7 +38,9 @@ public class ManageVehicleUIController implements Initializable{
 
     @FXML
     private void addAction() {
+        VehicleDTO vehicleDTO = new VehicleDTO(
 
+        );
     }
 
     @FXML
@@ -50,10 +63,35 @@ public class ManageVehicleUIController implements Initializable{
     }
 
     @FXML
-    private void browseImgURL(){}
+    private void browseImgURL(){
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(fileChooseStage);
+
+        if (file != null){
+            fileName = String.valueOf(file);
+            String mimeType = null;
+            try {
+                mimeType = Files.probeContentType(file.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (mimeType != null && mimeType.split("/")[0].equals("image")) {
+                fileName = fileName.substring(24,fileName.length());
+                fileChooser.showSaveDialog(fileChooseStage);
+            }else {
+                new AlertBuilder("error","Browse","Image",
+                        "Reception couldn't delete !!");
+            }
+        }else {
+            System.out.println("File is null");
+        }
+    }
 
     @FXML
-    private void removeImgURL(){}
+    private void removeImgURL(){
+        imgVehicle.setImage(new Image("/com/chamodshehanka/rentLioClient/view/images/notAvailable.png"));
+    }
 
     @FXML
     private void getSelectedItem() {
