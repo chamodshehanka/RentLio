b9175f11278faec72ad586abdbd68bf0ec507c2b
Shehanka <hcsperera@outlook.com>
2018-06-19 23:25:17 +0530

    developing reservation controller

--- RentLioClient/src/com/chamodshehanka/rentLioClient/view/controller/ManageReservationUIController.java
+++ RentLioClient/src/com/chamodshehanka/rentLioClient/view/controller/ManageReservationUIController.java
@@ -14,6 +14,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -248,8 +249,9 @@ public class ManageReservationUIController implements Initializable{
     }
 
     @FXML
-    private void checkDate(){
-
+    private void checkDateAction(){
+        LocalDate localDate = dpkPickUpDate.getValue();
+        System.out.println(localDate);
     }
 
 }
