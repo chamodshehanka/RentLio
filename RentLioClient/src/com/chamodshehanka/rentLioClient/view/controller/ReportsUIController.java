5f8d5ce92c8ccb91af3bbfb992f8eee7e36fc8bf
Shehanka <hcsperera@outlook.com>
2018-06-27 20:23:30 +0530

    TopVehicleUI with sceneManager.

--- RentLioClient/src/com/chamodshehanka/rentLioClient/view/controller/ReportsUIController.java
+++ RentLioClient/src/com/chamodshehanka/rentLioClient/view/controller/ReportsUIController.java
@@ -1,11 +1,10 @@
 package com.chamodshehanka.rentLioClient.view.controller;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import lk.vivoxalabs.customstage.CustomStage;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,14 +24,7 @@ public class ReportsUIController implements Initializable{
 
     @FXML
     private void loadTopVehicleUI(){
-        try {
-            AnchorPane anchorPane = FXMLLoader.load(getClass()
-                    .getResource("/com/chamodshehanka/rentLioClient/view/fxml/TopVehiclesUI.fxml"));
-            rootReports.getChildren().setAll(anchorPane);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-       /* AnchorPane topVehicleUIPane = CustomStage.getDefaultSceneManager().getScene("ReportsUI");
-        rootReports.getChildren().setAll(topVehicleUIPane);*/
+        AnchorPane topVehicleUIPane = CustomStage.getDefaultSceneManager().getScene("TopVehiclesUI");
+        rootReports.getChildren().setAll(topVehicleUIPane);
     }
 }
