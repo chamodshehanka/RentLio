5510baa75ead3a377c20565e539b5d0c9cdaf8f1
Shehanka <hcsperera@outlook.com>
2018-06-27 09:05:48 +0530

    Top rent vehicles UI developing

---
+++ RentLioClient/src/com/chamodshehanka/rentLioClient/view/controller/TopVehiclesUIController.java
@@ -0,0 +1,18 @@
+package com.chamodshehanka.rentLioClient.view.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author chamodshehanka on 6/27/2018
+ * @project RentLio
+ **/
+public class TopVehiclesUIController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
