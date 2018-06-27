5d3964a86e89cdb7c21a6819b1d9e4697e60c629
Shehanka <hcsperera@outlook.com>
2018-06-27 20:36:46 +0530

    Add sample data to TopVehicle Pie Chart

--- RentLioClient/src/com/chamodshehanka/rentLioClient/view/controller/TopVehiclesUIController.java
+++ RentLioClient/src/com/chamodshehanka/rentLioClient/view/controller/TopVehiclesUIController.java
@@ -2,6 +2,8 @@ package com.chamodshehanka.rentLioClient.view.controller;
 
 import com.chamodshehanka.rentLioClient.util.AlertBuilder;
 import com.jfoenix.controls.JFXComboBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
@@ -32,8 +34,28 @@ public class TopVehiclesUIController implements Initializable{
 
         //switch by vehicle type
         switch (chartType){
-            case 1 : break;
-            case 2 : break;
+            case 1 :
+                    ObservableList<PieChart.Data> pieChartData =
+                    FXCollections.observableArrayList(
+                            new PieChart.Data("Prius", 13),
+                            new PieChart.Data("Audi A7", 25),
+                            new PieChart.Data("Audi R8", 10),
+                            new PieChart.Data("BMW i8", 22),
+                            new PieChart.Data("Premio", 30));
+
+                    topVehiclesChart.setData(pieChartData);
+                    topVehiclesChart.setTitle("Imported Fruits");
+                break;
+            case 2 :
+                pieChartData = FXCollections.observableArrayList(
+                        new PieChart.Data("Prius", 73),
+                        new PieChart.Data("Audi A7", 25),
+                        new PieChart.Data("Audi R8", 17),
+                        new PieChart.Data("BMW i8", 45),
+                        new PieChart.Data("Premio", 90));
+
+                topVehiclesChart.setData(pieChartData);
+                break;
             case 3 : break;
             default : new AlertBuilder("warn","Top Vehicles","Top Vehicle Graph",
                     "Invalid type of graph !!");
