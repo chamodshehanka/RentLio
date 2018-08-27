461ff4bf064737f161ef4ecd32500b2092f2bf4c
Shehanka <hcsperera@outlook.com>
2018-08-27 22:51:29 +0530

    Payment UI created

---
+++ RentLioClient/src/com/chamodshehanka/rentLioClient/view/controller/PaymentUIController.java
@@ -0,0 +1,18 @@
+package com.chamodshehanka.rentLioClient.view.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author chamodshehanka on 8/27/2018
+ * @project RentLio
+ **/
+public class PaymentUIController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
